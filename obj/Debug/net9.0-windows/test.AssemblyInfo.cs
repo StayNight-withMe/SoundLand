e55f57ae10aadfd351a200d7958a210acd097826
Виктор <kasa.king_over@mail.ru>
2025-10-17 23:32:25 +0300

    style changed

--- obj/Debug/net9.0-windows/test.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4752f7e40563cd2a5db6b71a4f6ef11c219f70f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+573d6237145be4935cf0e525b02db76f9b9ce540")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
